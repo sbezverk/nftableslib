5f5b94ea71da75f60b2cddd0a0a97e4f3cb9556e
Serguei Bezverkhi <sbezverk@cisco.com>
2019-05-15 11:22:00 -0700

    Add Delete function and test
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- nftables.go
+++ nftables.go
@@ -9,7 +9,8 @@ import (
 // NFTables defines an interface
 type NFTables interface {
 	AddNFTable(name string, familyType nftables.TableFamily)
-	NFTableExistAndProgrammed(name string, familyType nftables.TableFamily) (bool, bool)
+	DeleteNFTable(name string, familyType nftables.TableFamily)
+	NFTableExist(name string, familyType nftables.TableFamily) bool
 }
 
 type nfTables struct {
@@ -20,8 +21,7 @@ type nfTables struct {
 }
 
 type nfTable struct {
-	table      *nftables.Table
-	programmed bool
+	table *nftables.Table
 }
 
 // InitConn initializes netlink connection of the nftables family
@@ -44,11 +44,8 @@ func (nft *nfTables) AddNFTable(name string, familyType nftables.TableFamily) {
 	nft.Lock()
 	defer nft.Unlock()
 	// Check if nf table with the same family type and name  already exists
-	if t, ok := nft.tables[familyType][name]; ok {
-		if t.programmed {
-			// TODO Figure out what to do if a table with specified type and name already exists.
-			// Should the corresponding kernel table be deleted first?
-		}
+	if _, ok := nft.tables[familyType][name]; ok {
+		nft.Conn.DelTable(nft.tables[familyType][name].table)
 		// Removing old table, at this point, this table should be removed from the kernel as well.
 		delete(nft.tables[familyType], name)
 	}
@@ -58,17 +55,29 @@ func (nft *nfTables) AddNFTable(name string, familyType nftables.TableFamily) {
 	})
 	nft.tables[familyType] = make(map[string]*nfTable)
 	nft.tables[familyType][name] = &nfTable{
-		table:      t,
-		programmed: false,
+		table: t,
 	}
 }
 
-func (nft *nfTables) NFTableExistAndProgrammed(name string, familyType nftables.TableFamily) (bool, bool) {
-	if t, ok := nft.tables[familyType][name]; ok {
-		if t.programmed {
-			return true, true
-		}
-		return true, false
+// AddNFTable appends a table into NF tables list
+func (nft *nfTables) DeleteNFTable(name string, familyType nftables.TableFamily) {
+	nft.Lock()
+	defer nft.Unlock()
+	// Check if nf table with the same family type and name  already exists
+	if _, ok := nft.tables[familyType][name]; ok {
+		nft.Conn.DelTable(nft.tables[familyType][name].table)
+		// Removing old table, at this point, this table should be removed from the kernel as well.
+		delete(nft.tables[familyType], name)
 	}
-	return false, false
+	// If no more tables exists under a specific family name, removing  family type.
+	if len(nft.tables[familyType]) == 0 {
+		delete(nft.tables, familyType)
+	}
+}
+
+func (nft *nfTables) NFTableExist(name string, familyType nftables.TableFamily) bool {
+	if _, ok := nft.tables[familyType][name]; ok {
+		return true
+	}
+	return false
 }
--- nftables_test.go
+++ nftables_test.go
@@ -12,11 +12,21 @@ func TestAddNFTable(t *testing.T) {
 		t.Fatal("initialization of netlink connection failed")
 	}
 	conn.AddNFTable("filter", nftables.TableFamilyIPv4)
-	exist, programmed := conn.NFTableExistAndProgrammed("filter", nftables.TableFamilyIPv4)
+	exist := conn.NFTableExist("filter", nftables.TableFamilyIPv4)
 	if !exist {
 		t.Fatalf("expected table %s of type %v to exist, but it does not", "filter", nftables.TableFamilyIPv4)
 	}
-	if programmed {
-		t.Fatalf("expected table %s of type %v to be not programmed, but it does", "filter", nftables.TableFamilyIPv4)
+}
+
+func TestDeleteNFTable(t *testing.T) {
+	conn := InitConn()
+	if conn == nil {
+		t.Fatal("initialization of netlink connection failed")
+	}
+	conn.AddNFTable("filter", nftables.TableFamilyIPv4)
+	conn.DeleteNFTable("filter", nftables.TableFamilyIPv4)
+	exist := conn.NFTableExist("filter", nftables.TableFamilyIPv4)
+	if exist {
+		t.Fatalf("expected table %s of type %v not exist, but it does", "filter", nftables.TableFamilyIPv4)
 	}
 }
