3efe27730b84db4384de03214a064b94b2eeb0d9
Serguei Bezverkhi <sbezverk@cisco.com>
2019-06-01 09:18:05 -0700

    Add initial L4 processing
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- nfrule_l4.go
+++ nfrule_l4.go
@@ -1,9 +1,47 @@
 package nftableslib
 
-import "github.com/google/nftables"
+import (
+	"fmt"
+
+	"github.com/google/nftables/expr"
+
+	"github.com/google/nftables"
+)
 
 func createL4(rule *L4Rule, set nftables.Set) (*nftables.Rule, []nftables.SetElement, error) {
 	r := nftables.Rule{}
 	s := make([]nftables.SetElement, 0)
+
+	var rulePort *Port
+	var offset uint32
+
+	if rule.Src != nil {
+		rulePort = rule.Src
+		offset = 0
+	}
+	if rule.Dst != nil {
+		rulePort = rule.Dst
+		offset = 2
+	}
+	if rulePort == nil {
+		return nil, nil, fmt.Errorf("both source and destination ports are nil")
+	}
+
+	if rule.Redirect != nil {
+		return processPortRedirect(rule.L4Proto, offset, rulePort, *rule.Redirect)
+	}
+	if rule.Verdict != nil {
+		return processL4Port(rule.L4Proto, offset, rulePort, rule.Exclude, rule.Verdict)
+	}
 	return &r, s, nil
+}
+
+func processPortRedirect(l4proto int, offset uint32, port *Port, redicrect uint32) (*nftables.Rule, []nftables.SetElement, error) {
+
+	return nil, nil, fmt.Errorf("not implemented")
+}
+
+func processL4Port(l4proto int, offset uint32, port *Port, exclude bool, verdict *expr.Verdict) (*nftables.Rule, []nftables.SetElement, error) {
+
+	return nil, nil, fmt.Errorf("not implemented")
 }
