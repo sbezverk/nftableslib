98614b0d4b41f94fcf3868634a85f27ed7bdd565
Serguei Bezverkhi <sbezverk@cisco.com>
2019-06-07 08:53:45 -0700

    simpleipv4 rule
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

---
+++ samples/simpleipv4rule/go.mod
@@ -0,0 +1,9 @@
+module simpleipv4rule
+
+go 1.12
+
+require (
+	github.com/google/nftables v0.0.0-20190605111505-8d26daf06058
+	github.com/sbezverk/nftableslib v0.0.0-20190606151428-ce5a3a1f1fe7
+	golang.org/x/sys v0.0.0-20190602015325-4c4f7f33c9ed
+)
---
+++ samples/simpleipv4rule/simpleipv4rule.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/google/nftables"
+	"github.com/google/nftables/expr"
+	"github.com/sbezverk/nftableslib"
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	// Initializing netlink connecrtion
+	conn := nftableslib.InitConn()
+	ti := nftableslib.InitNFTables(conn)
+	fmt.Printf("Cleaning nftable...\n")
+	conn.FlushRuleset()
+
+	ti.Tables().Create("ipv4table", nftables.TableFamilyIPv4)
+	fmt.Printf("Programming nftable...\n")
+	if err := conn.Flush(); err != nil {
+		fmt.Printf("Failed to programm nftable with error: %+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Programming nftable succeeded.\n")
+
+	ti.Tables().Table("ipv4table", nftables.TableFamilyIPv4).Chains().Create("ipv4chain-1", nftables.ChainHookPrerouting,
+		nftables.ChainPriorityFirst, nftables.ChainTypeFilter)
+	fmt.Printf("Programming chain...\n")
+	if err := conn.Flush(); err != nil {
+		fmt.Printf("Failed to programm nftable with error: %+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Programming chain succeeded.\n")
+	port2 := uint32(8989)
+	// rule1Mask := uint8(25)
+	rule1 := nftableslib.Rule{
+		/*		L3: &nftableslib.L3Rule{
+				Dst: &nftableslib.IPAddrSpec{
+					List: []*nftableslib.IPAddr{
+						{
+							&net.IPAddr{
+								IP: net.ParseIP("192.168.20.0"),
+							},
+							false,
+							nil, // &rule1Mask,
+						},
+					},
+				},
+				Verdict: &expr.Verdict{
+					Kind: expr.VerdictKind(unix.NFT_JUMP),
+				},
+			}, */
+		L4: &nftableslib.L4Rule{
+			L4Proto: unix.IPPROTO_UDP,
+			Src: &nftableslib.Port{
+				List: []*uint32{
+					&port2,
+				},
+			},
+			Verdict: &expr.Verdict{
+				Kind: expr.VerdictKind(unix.NFT_RETURN),
+			},
+		},
+	}
+	if err := ti.Tables().Table("ipv4table", nftables.TableFamilyIPv4).Chains().Chain("ipv4chain-1").Rules().Create("ipv4rule-1", &rule1); err != nil {
+		fmt.Printf("failed to create chain with error: %+v, exiting...\n", err)
+		os.Exit(1)
+	}
+	jsontables, err := ti.Tables().Dump()
+	if err != nil {
+		fmt.Printf("failed to dump tables content with error: %+v\n", err)
+	}
+	fmt.Printf("Internal dump: %s\n", string(jsontables))
+
+	fmt.Printf("Programming nftable...\n")
+	if err := conn.Flush(); err != nil {
+		fmt.Printf("Failed to programm nftable with error: %+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Programming nftable succeeded.\n")
+
+	tables, err := conn.ListTables()
+	if err != nil {
+		fmt.Printf("failed to list tables with error: %+v\n", err)
+	}
+	fmt.Printf("netlink tables list: %+v\n", tables)
+}
