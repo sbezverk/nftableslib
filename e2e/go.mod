dc4cacec32b595c80a3b22be2bbbc67fd37664ac
Serguei Bezverkhi <sbezverk@cisco.com>
2019-10-09 11:42:10 -0400

    Add simple ICMP blocking test
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- e2e/go.mod
+++ e2e/go.mod
@@ -3,8 +3,9 @@ module github.com/sbezverk/nftableslib/e2e
 go 1.13
 
 require (
+	github.com/google/nftables v0.0.0-20190906062827-5d14089d2edc
+	github.com/sbezverk/nftableslib v0.0.0-20191009141231-43ad06cfdea0
 	github.com/sbezverk/nftableslib/e2e/setenv v0.0.0
 	github.com/vishvananda/netns v0.0.0-20190625233234-7109fa855b0f
+	golang.org/x/sys v0.0.0-20191008105621-543471e840be
 )
-
-replace github.com/sbezverk/nftableslib/e2e/setenv => ./setenv
--- e2e/main.go
+++ e2e/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 
+	"golang.org/x/sys/unix"
+
+	"github.com/google/nftables"
+	"github.com/sbezverk/nftableslib"
 	"github.com/sbezverk/nftableslib/e2e/setenv"
+	"github.com/vishvananda/netns"
 )
 
 func main() {
@@ -13,5 +19,70 @@ func main() {
 		fmt.Printf("Failed with error: %+v\n", err)
 		os.Exit(1)
 	}
-	t.Cleanup()
+	defer t.Cleanup()
+	ns := t.GetNamespace()
+	ips := t.GetIPs()
+	if err := setenv.TestICMP(t.GetNamespace()[0], unix.IPPROTO_ICMP, ips[0], ips[1]); err != nil {
+		fmt.Printf("Connectivity test supposed to succeed, but failed with error: %+v\n", err)
+		os.Exit(1)
+	}
+	if err := nftablesSet(ns[1], ips[1]); err != nil {
+		fmt.Printf("Failed with error: %+v\n", err)
+		os.Exit(1)
+	}
+	if err := setenv.TestICMP(t.GetNamespace()[0], unix.IPPROTO_ICMP, ips[0], ips[1]); err == nil {
+		fmt.Printf("Connectivity test supposed to fail, but succeeded\n")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Test succeeded!\n")
+}
+
+func nftablesSet(ns netns.NsHandle, dip *net.IPNet) error {
+	// Initializing netlink connection
+	conn := nftableslib.InitConn(int(ns))
+	ti := nftableslib.InitNFTables(conn)
+
+	if err := ti.Tables().CreateImm("table", nftables.TableFamilyIPv4); err != nil {
+		return fmt.Errorf("failed to create table with error: %+v", err)
+	}
+	ci, err := ti.Tables().Table("table", nftables.TableFamilyIPv4)
+	if err != nil {
+		return fmt.Errorf("failed to get chains interface for table with error: %+v", err)
+	}
+	ch1Attr := nftableslib.ChainAttributes{
+		Type:     nftables.ChainTypeFilter,
+		Priority: 0,
+		Hook:     nftables.ChainHookInput,
+		Policy:   nftableslib.ChainPolicyAccept,
+	}
+	if err := ci.Chains().CreateImm("chain-1", &ch1Attr); err != nil {
+		return fmt.Errorf("failed to create chain with error: %+v", err)
+	}
+
+	c1ri, err := ci.Chains().Chain("chain-1")
+	if err != nil {
+		return fmt.Errorf("failed to get rules interface for chain with error: %+v", err)
+	}
+	addr, err := nftableslib.NewIPAddr(dip.IP.String())
+	if err != nil {
+		return err
+	}
+	action, err := nftableslib.SetVerdict(nftableslib.NFT_DROP)
+	if err != nil {
+		return err
+	}
+	rule := nftableslib.Rule{
+		L3: &nftableslib.L3Rule{
+			Protocol: nftableslib.L3Protocol(unix.IPPROTO_ICMP),
+			Dst: &nftableslib.IPAddrSpec{
+				List: []*nftableslib.IPAddr{addr},
+			},
+		},
+		Action: action,
+	}
+	if _, err = c1ri.Rules().CreateImm(&rule); err != nil {
+		return fmt.Errorf("failed to create rule with error: %+v", err)
+	}
+	return nil
 }
