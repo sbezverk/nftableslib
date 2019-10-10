bc9ba6cbfa19ade0466a6520748d4325d6352dd1
Serguei Bezverkhi <sbezverk@cisco.com>
2019-10-10 11:47:32 -0400

    Attempting to fix table creation failure#2
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- e2e/main.go
+++ e2e/main.go
@@ -135,7 +135,7 @@ func nftablesSet(ns netns.NsHandle, version nftables.TableFamily, nfrules map[nf
 	conn := nftableslib.InitConn(int(ns))
 	ti := nftableslib.InitNFTables(conn)
 
-	tn := uuid.New().String()[:16]
+	tn := uuid.New().String()[:8]
 	fmt.Printf("Table name: %s\n", tn)
 	if err := ti.Tables().CreateImm(tn, version); err != nil {
 		return fmt.Errorf("failed to create table with error: %+v", err)
