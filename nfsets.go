5bdec46dadb14a1d0614df69b447a4817476abe6
Serguei Bezverkhi <sbezverk@cisco.com>
2019-11-29 15:47:42 -0500

    Add Concat to real
    Signed-off-by: Serguei Bezverkhi <sbezverk@cisco.com>

--- nfsets.go
+++ nfsets.go
@@ -294,3 +294,38 @@ func processElementValue(keyT nftables.SetDatatype, keyV ElementValue) ([]byte, 
 
 	return b, nil
 }
+
+// SetConcateTypeBits defines concatinatio bits, originally defined in
+// https://git.netfilter.org/iptables/tree/iptables/nft.c#n999
+const SetConcateTypeBits = 6
+
+// GenSetKeyType generates a composite key type, combining all types
+func GenSetKeyType(types ...nftables.SetDatatype) nftables.SetDatatype {
+	switch len(types) {
+	case 0:
+		return nftables.TypeInvalid
+	case 1:
+		return types[0]
+	default:
+		c := types[0].NFTMagic
+		b := types[0].Bytes
+		name := types[0].Name + "_"
+		for i := 1; i < len(types); i++ {
+			c = c<<SetConcateTypeBits | types[i].NFTMagic
+			b += types[i].Bytes
+			name += types[i].Name
+			if i < len(types) {
+				name += "_"
+			}
+		}
+		// Alignment to 4 bytes
+		if b%4 != 0 {
+			b += 4 - (b % 4)
+		}
+		return nftables.SetDatatype{
+			Name:     name,
+			Bytes:    b,
+			NFTMagic: c,
+		}
+	}
+}
